b9ec80b02e903712aaa4797d375ca1e13efb0111
Diana Barsan <barsan@medicmobile.org>
2020-07-10 14:13:28 +0300

    Only resume listening when queue is not paused.

--- sentinel/src/lib/feed.js
+++ sentinel/src/lib/feed.js
@@ -54,7 +54,7 @@ const registerFeed = seq => {
     .on('error', err => {
       logger.error('transitions: error from changes feed: %o', err);
       request = null;
-      setTimeout(() => listen(), RETRY_TIMEOUT);
+      setTimeout(() => resumeProcessing(), RETRY_TIMEOUT);
     });
   return request;
 };
@@ -134,17 +134,22 @@ const changeQueue = async.queue((change, callback) => {
   });
 });
 
+const resumeProcessing = () => {
+  if (request || changeQueue.paused) {
+    return;
+  }
+  logger.info('transitions: processing enabled');
+  return getProcessedSeq().then(seq => registerFeed(seq));
+};
+
 changeQueue.drain(() => {
   logger.debug(`transitions: queue drained`);
-  listen();
+  resumeProcessing();
 });
 
 const listen = () => {
   changeQueue.resume();
-  if (!request) {
-    logger.info('transitions: processing enabled');
-    return getProcessedSeq().then(seq => registerFeed(seq));
-  }
+  return resumeProcessing();
 };
 
 module.exports = {
